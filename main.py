39adee3d5ec7a77e067bd1bfd731d95ac62aab2b
Siddarth Tamse <sidtamse7@gmail.com>
2024-05-09 20:40:01 +0530

    fruits,vegetables,dairy code updated

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -33,4 +35,20 @@ async def get_current_user():
     return {"message": "this is the current user"}
 
 
+class FoodEnum(str, Enum):
+    fruits = "fruits"
+    vegetables = "vegetables"
+    dairy = "dairy"
 
+
+@app.get("/foods/{food_name}")
+async def get_food(food_name: FoodEnum):
+    if food_name == FoodEnum.vegetables:
+        return {"food_name": food_name, "message": "you are healthy"}
+
+    if food_name.value == "fruits":
+        return {
+            "food_name": food_name,
+            "message": "you are still healthy, but like sweet things",
+        }
+    return {"food_name": food_name, "message": "i like chocolate milk"}
