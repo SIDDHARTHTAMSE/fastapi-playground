7e149bfe96773c0ccee19472ca8bcfbb89ac585a
Siddarth Tamse <sidtamse7@gmail.com>
2024-05-10 20:00:55 +0530

    added some api code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Optional
 
 from fastapi import FastAPI
 
@@ -39,6 +40,7 @@ class FoodEnum(str, Enum):
     fruits = "fruits"
     vegetables = "vegetables"
     dairy = "dairy"
+    icecream = "ICE CREAM"
 
 
 @app.get("/foods/{food_name}")
@@ -52,3 +54,43 @@ async def get_food(food_name: FoodEnum):
             "message": "you are still healthy, but like sweet things",
         }
     return {"food_name": food_name, "message": "i like chocolate milk"}
+
+
+fake_items_db = [
+    {"items_names": "Foo"},
+    {"items_name": "Bar"},
+    {"items_name": "Baz"}
+]
+
+
+@app.get("/items")
+async def list_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def get_items(item_id: str, sample_query_param: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id, "sample_query_param": sample_query_param}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({
+            "description": " Transform writing with efficiency and creativity create, comprehend, refine, and elevate your documents "
+        })
+    return item
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def get_user_item(
+        user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {
+                "description": " Transform writing with efficiency and creativity create, comprehend, refine, and elevate your documents "
+            }
+        )
+    return item
