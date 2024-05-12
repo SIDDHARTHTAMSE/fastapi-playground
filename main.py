8b074c2125e1712a36e97b4c53886ca1e199c805
Siddarth Tamse <sidtamse7@gmail.com>
2024-05-12 13:37:29 +0530

    Pushing code about query()

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from typing import Optional
+from fastapi import FastAPI, Query
 
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -58,15 +59,15 @@ async def get_food(food_name: FoodEnum):
 
 
 fake_items_db = [
-    {"items_names": "Foo"},
+    {"items_name": "Foo"},
     {"items_name": "Bar"},
     {"items_name": "Baz"}
 ]
 
 
-@app.get("/items")
+@app.get("/itemsS")
 async def list_items(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
+    return fake_items_db[skip: skip + limit]
 
 
 @app.get("/items/{item_id}")
@@ -104,18 +105,52 @@ class Item(BaseModel):
     tax: float | None = None
 
 
+class ItemResponse(BaseModel):
+    res_name: str
+    res_description: str | None = None
+    res_price: float
+    res_tax: float | None = None
+    total_price: float | None = None
+
+
 @app.post("/items")
 async def create_item(item: Item):
-    item_dict = item.dict()
-    if item.tax:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
+    result = ItemResponse(
+        res_name=item.name,
+        res_description=item.description,
+        res_price=item.price,
+        res_tax=item.tax,
+        total_price=item.price + item.tax
+    )
+    return result
+    # item_dict = item.dict()
+    # if item.tax:
+    #     item_dict.update(
+    #         {"price_with_tax": item.price + item.tax}
+    #     )
+    # return item_dict
 
 
 @app.put("/items/{item_id}")
 async def create_item_with_put(item_id: int, item: Item, q: str | None = None):
-    result = {"item_id": item_id, **item.dict()}
+    result = {
+        "item_id": item_id, **item.dict()
+    }
     if q:
         result.update({"q": q})
     return result
+
+
+@app.get("/itemsp")
+async def read_items(p: str | None = Query(None, min_length=3,max_length=10,title="Simple query string",description="This is a sample string")):
+    results = {"items": [{"items_id": "Foo"}, {"items_id": "Bar"}]}
+    if p:
+        results.update({"p": p})
+    return results
+
+
+@app.get("/items_/hidden")
+async def hidden_query_route(hidden_query: str | None = Query(None, include_in_schema=False)):
+    if hidden_query:
+        return {"hidden_query": hidden_query}
+    return {"hidden_query": "Not found"}
